1c6329e540e4e916871751ea3f56ff13a3408668
Karl <karlcampbell100@gmail.com>
2025-02-10 16:31:05 +0000

    created newDeck method to loop through suits and symbols to create each individual card when a new deck is initialised!

--- untitled/src/main/java/CardGame/CardGame.java
+++ untitled/src/main/java/CardGame/CardGame.java
@@ -3,11 +3,33 @@ package CardGame;
 import java.util.ArrayList;
 
 public class CardGame {
-    private ArrayList<Card> deckOfCards;
 
-    public CardGame(){
-        deckOfCards = new ArrayList<>();
-        createDeck();
+    private ArrayList<Card> deckOfCards;
+//    private String name;
+
+    public CardGame() {
+        this.deckOfCards = new ArrayList<>();
+        newDeck();
+    }
+
+        private void newDeck(){
+            String[] suits = {"♠", "♢", "♡", "♣"};
+            String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+            int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+            for(String suit: suits){
+                for(int currentSymbol = 0; currentSymbol < symbols.length; currentSymbol++){
+                    deckOfCards.add(new Card(suit, symbols[currentSymbol], values[currentSymbol]));
+                }
+            }
+        }
+
+    
+
+
+
+
+
     }
 
 //    STAGE 1
@@ -17,7 +39,7 @@ public class CardGame {
 
 //    getDeck() Method. This will list out all the cards in the deck in its current order
 
-//    Cards have a String (suit) this will use the unicode characters found in a deck (♠ ♥ ♦ ♣ OR ♤ ♡ ♢ ♧)
+//    Cards have a String (suit) this will use the unicode characters found in a deck ((♠ ♥ ♦ ♣ OR ♤ ♡ ♢ ♧)
 
 //    each card has a String symbol. this is what is found on the card (A 2 3 4 5 6 7 8 9 10 J Q K)
 
@@ -28,4 +50,3 @@ public class CardGame {
 //    STAGE 2
     
 
-}
