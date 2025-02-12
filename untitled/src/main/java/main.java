cb17cb6c51218fe147d2a3f5abf795a568329005
Karl <karlcampbell100@gmail.com>
2025-02-12 18:56:38 +0000

    confirming each sorting method works by passing these at game start in main Java

--- untitled/src/main/java/main.java
+++ untitled/src/main/java/main.java
@@ -3,6 +3,7 @@ import CardGame.CardGame;
 public class main {
     public static void main(String[] args){
         CardGame game = new CardGame();
+        game.shuffleDeck();
         game.getDeck();
         System.out.println(game.dealCard());
     }
