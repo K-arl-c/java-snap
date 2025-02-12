9a24bf5cd74709fe59d52cd14639006dd2a82551
Karl <karlcampbell100@gmail.com>
2025-02-12 18:54:53 +0000

    created sorting per number, suits and randomly

--- untitled/src/main/java/CardGame/CardGame.java
+++ untitled/src/main/java/CardGame/CardGame.java
@@ -1,48 +1,60 @@
 package CardGame;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class CardGame {
 
     private ArrayList<Card> deckOfCards;
-//    private String name;
+
+    //    private String name;
 //TO DO CREATE CARD GAME NAME
     public CardGame() {
         this.deckOfCards = new ArrayList<>();
         newDeck();
     }
 
-        private void newDeck(){
-            String[] suits = {"♠", "♢", "♡", "♣"};
-            String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-            int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    private void newDeck() {
+        String[] suits = {"♠", "♢", "♡", "♣"};
+        String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
-            for(String suit: suits){
-                for(int currentSymbol = 0; currentSymbol < symbols.length; currentSymbol++){
-                    deckOfCards.add(new Card(suit, symbols[currentSymbol], values[currentSymbol]));
-                }
+        for (String suit : suits) {
+            for (int currentSymbol = 0; currentSymbol < symbols.length; currentSymbol++) {
+                deckOfCards.add(new Card(suit, symbols[currentSymbol], values[currentSymbol]));
             }
         }
-
-
-        public void getDeck() {
-            for (Card card : deckOfCards) {
-                System.out.println(card);
-            }
-        }
-
-        public Card dealCard(){
-        return deckOfCards.removeFirst();
-        }
-
-
-
-//        SORTING
-    public ArrayList<Card> sortDeckInNumberOrder(){
-        deckOfCards.sort(Comparator.comparingInt(Card::getValue));
     }
+
+
+    public void getDeck() {
+        for (Card card : deckOfCards) {
+            System.out.println(card);
         }
+    }
+
+    public Card dealCard() {
+        return deckOfCards.removeFirst();
+    }
+
+
+    //        SORTING
+    public ArrayList<Card> sortDeckInNumberOrder() {
+        deckOfCards.sort(Comparator.comparingInt(Card::getValue));
+        return deckOfCards;
+    }
+
+    public ArrayList<Card> sortDeckIntoSuits() {
+        deckOfCards.sort(Comparator.comparing(Card::getSuit));
+        return deckOfCards;
+    }
+
+    public ArrayList<Card> shuffleDeck() {
+        Collections.shuffle(deckOfCards);
+        return deckOfCards;
+    }
+}
 
 //    STAGE 1
 //    Array List for Cards for deckOfCards - this will contain 52 cards
