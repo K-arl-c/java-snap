298b7d7ca00fbeb6da397c42cd4233bbdd21340f
Karl <karlcampbell100@gmail.com>
2025-02-14 15:18:41 +0000

    added Symbol matching logic to end game if symbols match

--- untitled/src/main/java/CardGame/Snap.java
+++ untitled/src/main/java/CardGame/Snap.java
@@ -9,11 +9,20 @@ public class Snap extends CardGame{
     public void playGame(){
         Scanner scanner = new Scanner(System.in);
         Card currentCard;
+        Card previousCard = null;
 
         while (!deckOfCards.isEmpty()) {
             scanner.nextLine();
             currentCard = dealCard();
             System.out.println("Drawn Card = " + currentCard);
+
+
+            if (currentCard.getSymbol().equals(previousCard.getSymbol())) {
+                System.out.println("Winner!");
+                break;
+            }
+            previousCard = currentCard;
         }
+        
     }
 }
